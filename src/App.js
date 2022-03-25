88b5772f35abf1ccf59c25bb341c8cd9124cef9b
ccooper2382 <93221305+ccooper2382@users.noreply.github.com>
2022-03-25 15:09:46 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-
 import './App.css';
+import {Component} from "react";
+import {Redirect, Route, Switch} from "react-router-dom";
 import Movies from "./Components/movies";
+import Customers from "./Components/customers";
+import NotFound from "./Components/notFound";
+import Rentals from "./Components/rentals";
+import NavBar from "./Components/navBar";
+import MovieForm from "./Components/movieForm";
+import LoginForm from "./Components/loginForm";
 
-function App() {
-  return (
-  <main className="container">
-    <Movies />
-  </main>
-  );
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar/>
+                <main className="container">
+                    <Switch>
+                        <Route path="/login" component={LoginForm} />
+                        <Route path="/movies/:id" component={MovieForm} />
+                        <Route path="/movies" component={Movies}/>
+                        <Route path="/customers" component={Customers}/>
+                        <Route path="/rentals" component={Rentals}/>
+                        <Route path="/notFound" component={NotFound}/>
+                        <Redirect from="/" exact to="/movies"/>
+                        <Redirect to="/not-found"/>
+                    </Switch>
+                </main>
+            </div>
+        )
+
+    }
 }
 
 export default App;
